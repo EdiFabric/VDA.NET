d3bc89c430cc90e846b830513d40b47f24094409
Kamen Nikolov <kamen.nikolov@edifabric.com>
2023-09-25 14:19:38 +0100

    Updated to EdiFabric 10.6.7

--- NET Framework 4.8/EdiFabric.Examples.VDA.Read/Program.cs
+++ NET Framework 4.8/EdiFabric.Examples.VDA.Read/Program.cs
@@ -1,4 +1,5 @@
 ﻿using EdiFabric.Examples.VDA.Common;
+using System;
 
 namespace EdiFabric.Examples.VDA.Read
 {
@@ -6,7 +7,15 @@ namespace EdiFabric.Examples.VDA.Read
     {
         static void Main(string[] args)
         {
-            SerialKey.Set(Config.TrialSerialKey);
+            try
+            {
+                SerialKey.Set(Config.TrialSerialKey, true);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.StartsWith("Can't set token"))
+                    throw new Exception("Your trial has expired! To continue using EdiFabric SDK you must purchase a plan from https://www.edifabric.com/pricing.html");
+            }
             //  Uncomment and then comment out the line above if you wish to use distributed cache for tokens
             //  TokenFileCache.Set();
 
--- NET Framework 4.8/EdiFabric.Examples.VDA.Write/Program.cs
+++ NET Framework 4.8/EdiFabric.Examples.VDA.Write/Program.cs
@@ -1,4 +1,5 @@
 ﻿using EdiFabric.Examples.VDA.Common;
+using System;
 
 namespace EdiFabric.Examples.VDA.Write
 {
@@ -6,7 +7,15 @@ namespace EdiFabric.Examples.VDA.Write
     {
         static void Main(string[] args)
         {
-            SerialKey.Set(Config.TrialSerialKey);
+            try
+            {
+                SerialKey.Set(Config.TrialSerialKey, true);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.StartsWith("Can't set token"))
+                    throw new Exception("Your trial has expired! To continue using EdiFabric SDK you must purchase a plan from https://www.edifabric.com/pricing.html");
+            }
 
             WriteVDA.Run();
             WriteVDABatch.Run();
